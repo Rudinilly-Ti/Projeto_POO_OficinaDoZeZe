b14d1fa0a05d1c1e4f05c1f5cd5bdeb74a19ec89
RicardoJun10r <ricardojunior0110@gmail.com>
2021-09-03 12:18:30 -0300

    adicionei ID na classe ServicoVO

--- Model/VO/ServicoVO.java
+++ Model/VO/ServicoVO.java
@@ -5,12 +5,13 @@ public class ServicoVO {
     // atributos
     private String nome;
     private double preco;
-    
+    private long id; 
 
     // construtor da classe
-    public ServicoVO(String nome, double preco){
+    public ServicoVO(String nome, double preco, long id){
         setNome(nome);
         setPreco(preco);
+        setId(id);
     }
 
     public ServicoVO(){
@@ -38,5 +39,14 @@ public class ServicoVO {
         if(preco > 0){
             this.preco = preco;
         }
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        if(!(id<0))
+            this.id = id;
     }
 }
