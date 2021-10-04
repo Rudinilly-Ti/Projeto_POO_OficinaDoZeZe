432260dc187e8a6fe225917785f6ff26fe7fab8e
PedroDavid2001 <pedrodavirochasaldanha@gmail.com>
2021-10-04 20:02:32 -0300

    Criando a primeira classe BO

--- Model/BO/AutomovelBO.java
+++ Model/BO/AutomovelBO.java
@@ -1,5 +1,136 @@
 package Model.BO;
 
-public class AutomovelBO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import Exceptions.*;
+import Model.DAO.AutomovelDAO;
+import Model.VO.AutomovelVO;
+
+public class AutomovelBO implements BaseInterBO<AutomovelVO>{
+    AutomovelDAO dao = new AutomovelDAO();
+    
+    //inserir
+    public void inserir(AutomovelVO vo) throws InsertException{
+        ResultSet rs = dao.findByPlaca(vo);
+        try {
+            if (rs.next()) {
+                throw new InsertException("Impossível cadastrar, pois já existe automóvel com essa placa.\n");
+            }
+            else{
+                dao.inserir(vo);
+            }
+        } catch (SQLException e) {
+            throw new InsertException(e.getMessage());
+        }
+    }
+
+    //listagem
+    public List<AutomovelVO> buscarPorId (AutomovelVO vo) throws FindException{
+        ResultSet rs = dao.findById(vo);
+        List<AutomovelVO> lista = new ArrayList<AutomovelVO>();
+        AutomovelVO vo2 = new AutomovelVO();
+        try {
+            if (!rs.next()) {
+                throw new FindException("Não foi encotrado nenhum carro com esse Id.\n");
+            }
+            else{
+                while(rs.next()){
+                    vo2.setID(rs.getLong("id"));
+                    vo2.setAno(rs.getInt("ano"));
+                    vo2.getCliente().setId(rs.getLong("id_cliente"));
+                    vo2.setCor(rs.getString("cor"));
+                    vo2.setMarca(rs.getString("marca"));
+                    vo2.setPlaca(rs.getString("placa"));
+                    vo2.setQuilometragem(rs.getDouble("quilometragem"));
+                    lista.add(vo2);
+                }
+            }
+        } catch (SQLException e) {
+            throw new FindException(e.getMessage());
+        }
+        return lista;
+    }
+
+    public List<AutomovelVO> buscarPorPlaca (AutomovelVO vo) throws FindException{
+        ResultSet rs = dao.findByPlaca(vo);
+        List<AutomovelVO> lista = new ArrayList<AutomovelVO>();
+        AutomovelVO vo2 = new AutomovelVO();
+        try {
+            if (!rs.next()) {
+                throw new FindException("Não foi encotrado nenhum carro com essa placa.\n");
+            }
+            else{
+                while(rs.next()){
+                    vo2.setID(rs.getLong("id"));
+                    vo2.setAno(rs.getInt("ano"));
+                    vo2.getCliente().setId(rs.getLong("id_cliente"));
+                    vo2.setCor(rs.getString("cor"));
+                    vo2.setMarca(rs.getString("marca"));
+                    vo2.setPlaca(rs.getString("placa"));
+                    vo2.setQuilometragem(rs.getDouble("quilometragem"));
+                    lista.add(vo2);
+                }
+            }
+        } catch (SQLException e) {
+            throw new FindException(e.getMessage());
+        }
+        return lista;
+    }
+
+    public List<AutomovelVO> buscarPorClienteId (AutomovelVO vo) throws FindException{
+        ResultSet rs = dao.findByClienteId(vo);
+        List<AutomovelVO> lista = new ArrayList<AutomovelVO>();
+        AutomovelVO vo2 = new AutomovelVO();
+        try {
+            if (!rs.next()) {
+                throw new FindException("Não foi encotrado nenhum carro com esse dono.\n");
+            }
+            else{
+                while(rs.next()){
+                    vo2.setID(rs.getLong("id"));
+                    vo2.setAno(rs.getInt("ano"));
+                    vo2.getCliente().setId(rs.getLong("id_cliente"));
+                    vo2.setCor(rs.getString("cor"));
+                    vo2.setMarca(rs.getString("marca"));
+                    vo2.setPlaca(rs.getString("placa"));
+                    vo2.setQuilometragem(rs.getDouble("quilometragem"));
+                    lista.add(vo2);
+                }
+            }
+        } catch (SQLException e) {
+            throw new FindException(e.getMessage());
+        }
+        return lista;
+    }
+        
+    public List<AutomovelVO> listar() throws FindException{
+        ResultSet rs = dao.listar();
+        List<AutomovelVO> lista = new ArrayList<AutomovelVO>();
+        AutomovelVO vo2 = new AutomovelVO();
+        try {
+            while(rs.next()){
+                vo2.setID(rs.getLong("id"));
+                vo2.setAno(rs.getInt("ano"));
+                vo2.getCliente().setId(rs.getLong("id_cliente"));
+                vo2.setCor(rs.getString("cor"));
+                vo2.setMarca(rs.getString("marca"));
+                vo2.setPlaca(rs.getString("placa"));
+                vo2.setQuilometragem(rs.getDouble("quilometragem"));
+                lista.add(vo2);
+            }
+        } catch (SQLException e) {
+            throw new FindException(e.getMessage());
+        }
+        return lista;
+    }
+
+    //Remoção por id
+    public void deletar(AutomovelVO vo){
+        dao.removerById(vo);
+    }
+
+    //Alteração
 
 }
--- Model/BO/baseInterBO.java
+++ Model/BO/baseInterBO.java
@@ -1,11 +1,11 @@
 package Model.BO;
 
-import java.sql.SQLException;
 import java.util.List;
+import Exceptions.*;
 
-public interface baseInterBO<VO> {
-  public void criar(VO entity) throws SQLException;
-  public VO buscar (VO entity) throws SQLException;
-  public List<VO> listar(VO entity) throws SQLException;
-  public void deletar(VO entity) throws SQLException;
+public interface BaseInterBO<VO>{
+    public void inserir(VO entity) throws InsertException;
+    public List<VO> buscarPorId(VO entity) throws FindException;
+    public List<VO> listar() throws FindException;
+    public void deletar(VO entity);
 }
