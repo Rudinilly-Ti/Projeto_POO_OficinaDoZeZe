9d516f413bf5598cd7566922499807ff12dea61f
PedroDavid2001 <pedrodavirochasaldanha@gmail.com>
2021-10-04 20:13:32 -0300

    Alterando alguns métodos findBy

--- Model/DAO/Teste.java
+++ Model/DAO/Teste.java
@@ -6,7 +6,6 @@ import Model.VO.*;
 
 public class Teste {
     public static void main(String[] args) {
-        BaseDAO dao = new BaseDAO();
-        System.out.println(dao.getConnection()); 
+         
     }
 }
