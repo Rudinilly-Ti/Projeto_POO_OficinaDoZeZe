08dfca76dcedfcbffbf2425fc319e681bc88c022
RicardoJun10r <ricardojunior0110@gmail.com>
2021-10-15 16:49:24 -0300

    criando os controles da tela de menu para o funcionario

---
+++ controller/MenuFuncionarioController.java
@@ -0,0 +1,53 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class MenuFuncionarioController {
+
+    @FXML
+    private Button botaoAutomoveis; // Direciona para buscar de automovel
+
+    @FXML
+    private Button botaoCliente; // Direciona para busca de cliente
+
+    @FXML
+    private Button botaoPecas; // Direciona para busca de peças
+
+    @FXML
+    private ImageView botaoSair; // sai do programa
+
+    @FXML
+    private Button botaoServico; // Direciona para busca de serviços
+
+    @FXML
+    private Label nomeDoUsuario; // aparece o nome do funcionario
+
+    @FXML
+    void PesquisarServicos(ActionEvent event) { // Direciona para busca de serviços
+
+    }
+
+    @FXML
+    void pesquisarCarros(ActionEvent event) { // Direciona para buscar de automovel
+
+    }
+
+    @FXML
+    void pesquisarCliente(ActionEvent event) { // Direciona para busca de cliente
+
+    }
+
+    @FXML
+    void pesquisarPecas(ActionEvent event) { // Direciona para busca de peças
+
+    }
+
+    @FXML
+    void sair(ActionEvent event) { // sai do programa
+
+    }
+
+}
