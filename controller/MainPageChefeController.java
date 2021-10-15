757291f306db7738ed5b71795eb4359e32b14e86
Rudinilly <rudinilly-ti@outlook.com>
2021-10-15 16:26:07 -0300

    Menu de usuario chefe

---
+++ controller/MainPageChefeController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainPageChefeController {
+
+    @FXML
+    private Button automovelButton;
+
+    @FXML
+    private Button clienteButton;
+
+    @FXML
+    private Button funcionarioButton;
+
+    @FXML
+    private Button orcamentoButton;
+
+    @FXML
+    private Button pecaButton;
+
+    @FXML
+    private Button servicoButton;
+
+    @FXML
+    void chamarTelaAutomoveis(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chamarTelaClientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chamarTelaFuncionarios(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chamarTelaOrcamentos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chamarTelaPecas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void chamarTelaServicos(ActionEvent event) {
+
+    }
+
+}
