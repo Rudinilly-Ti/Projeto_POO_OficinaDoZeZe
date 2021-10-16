ad4f2baff4705adf3e37b57eb5f3760f343aa7a8
RicardoJun10r <ricardojunior0110@gmail.com>
2021-10-15 22:03:54 -0300

    Criando tela de pesquisa de servicos

---
+++ view/TelaServicos.java
@@ -0,0 +1,22 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaServicos extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stg) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("VE/TelaServico.fxml"));
+        Scene cena = new Scene(root);
+        stg.setTitle("Oficina do Zezé");
+        stg.setScene(cena);
+        stg.show();
+    }
+}
