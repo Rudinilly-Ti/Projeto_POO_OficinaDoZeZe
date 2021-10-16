90e4b22230bc26ba3549e2159b5fa01b887ed8c9
RicardoJun10r <ricardojunior0110@gmail.com>
2021-10-15 22:16:40 -0300

    Criando controller de pecas

---
+++ controller/TelaPecasController.java
@@ -0,0 +1,93 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class TelaPecasController {
+    @FXML
+    private TextField Fabricante;
+
+    @FXML
+    private TextField NomePeca;
+
+    @FXML
+    private TextField PlacaAutomovel;
+
+    @FXML
+    private Button adicionarPeca;
+
+    @FXML
+    private Button botaoAutomoveis;
+
+    @FXML
+    private Button botaoCliente;
+
+    @FXML
+    private Button botaoFabricante;
+
+    @FXML
+    private Button botaoNomePeca;
+
+    @FXML
+    private Button botaoPecas;
+
+    @FXML
+    private Button botaoPesquisarPlaca;
+
+    @FXML
+    private Button botaoSair;
+
+    @FXML
+    private Button botaoServico;
+
+    @FXML
+    private Label nomeDoUsuario;
+
+    @FXML
+    void PesquisarServicos(ActionEvent event) { // redireciona para tela servicos
+
+    }
+
+    @FXML
+    void inserirPeca(ActionEvent event) { // cadastra outra peca
+
+    }
+
+    @FXML
+    void pesquisarCarros(ActionEvent event) { // redireciona para tela automoveis
+
+    }
+
+    @FXML
+    void pesquisarCliente(ActionEvent event) { // redireciona para tela clientes
+
+    }
+
+    @FXML
+    void pesquisarPecaPorFabricante(ActionEvent event) { // pesquisa peca
+
+    }
+
+    @FXML
+    void pesquisarPecaPorNome(ActionEvent event) { // pesquisa peça
+
+    }
+
+    @FXML
+    void pesquisarPecaPorPlaca(ActionEvent event) { // pesquisa peca
+
+    }
+
+    @FXML
+    void pesquisarPecas(ActionEvent event) { // acontece nada, pois já está na tela de peças
+
+    }
+
+    @FXML
+    void voltarParaMenu(ActionEvent event) { // volta para o menu
+
+    }
+}
