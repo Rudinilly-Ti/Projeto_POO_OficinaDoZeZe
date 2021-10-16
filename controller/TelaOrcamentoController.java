c626489c769b3dfc036fea15fa4e03aa3202ab3d
RicardoJun10r <ricardojunior0110@gmail.com>
2021-10-16 13:02:40 -0300

    Criando controller da tela orcamento

---
+++ controller/TelaOrcamentoController.java
@@ -0,0 +1,105 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class TelaOrcamentoController {
+    
+    @FXML
+    private TextField Placa;
+
+    @FXML
+    private TextField Placa1;
+
+    @FXML
+    private TextField Placa11;
+
+    @FXML
+    private Button adicionarOrcamento;
+
+    @FXML
+    private Button botaoAutomoveis;
+
+    @FXML
+    private Button botaoCliente;
+
+    @FXML
+    private Button botaoPecas;
+
+    @FXML
+    private Button botaoPesquisarCliente;
+
+    @FXML
+    private Button botaoPesquisarDataFinal;
+
+    @FXML
+    private Button botaoPesquisarDataInicial;
+
+    @FXML
+    private Button botaoPesquisarPlaca;
+
+    @FXML
+    private ImageView botaoSair;
+
+    @FXML
+    private Button botaoServico;
+
+    @FXML
+    private Button botaoServico1;
+
+    @FXML
+    private TextField dono;
+
+    @FXML
+    private Label nomeDoUsuario;
+
+    @FXML
+    void PesquisarServicos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inserirOrcamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarCarros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarDataFinal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarDataInicial(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarPecas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarPlaca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarParaMenu(ActionEvent event) {
+
+    }
+
+}
