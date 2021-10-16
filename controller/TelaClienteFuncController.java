3878104e411ba27fdcc529528a22938bd9c09b5a
RicardoJun10r <ricardojunior0110@gmail.com>
2021-10-15 22:16:05 -0300

    Criando controller de cliente no menu funcionario

---
+++ controller/TelaClienteFuncController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class TelaClienteFuncController {
+    @FXML
+    private Button adicionarCliente;
+
+    @FXML
+    private Button botaoAutomoveis;
+
+    @FXML
+    private Button botaoCliente;
+
+    @FXML
+    private Button botaoPecas;
+
+    @FXML
+    private Button botaoSair;
+
+    @FXML
+    private Button botaoServico;
+
+    @FXML
+    private Label nomeDoUsuario;
+
+    @FXML
+    void PesquisarServicos(ActionEvent event) { // redireciona para tela de serviços
+
+    }
+
+    @FXML
+    void inserirCliente(ActionEvent event) { // cadastra outro cliente
+
+    }
+
+    @FXML
+    void pesquisarCarros(ActionEvent event) { // redireciona para tela de automoveis
+
+    }
+
+    @FXML
+    void pesquisarCliente(ActionEvent event) { // acontece nada, pois já está na tela de cliente
+
+    }
+
+    @FXML
+    void pesquisarPecas(ActionEvent event) { // redireciona para tela de peças
+
+    }
+
+    @FXML
+    void voltarParaMenu(ActionEvent event) { // sai
+
+    }
+
+}
