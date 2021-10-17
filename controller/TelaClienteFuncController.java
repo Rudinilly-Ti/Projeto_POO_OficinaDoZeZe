e4936147dc3cbc073f123296eb90575aad5b54a7
RicardoJun10r <ricardojunior0110@gmail.com>
2021-10-16 23:18:42 -0300

    Adicionando controller de cliente para o menu funcionario

---
+++ controller/TelaClienteFuncController.java
@@ -0,0 +1,95 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+
+public class TelaClienteFuncController {
+    
+    @FXML
+    private Button adicionarCliente;
+
+    @FXML
+    private Button botaoAutomoveis;
+
+    @FXML
+    private Button botaoCadastrarCliente;
+
+    @FXML
+    private Button botaoCliente;
+
+    @FXML
+    private Button botaoPecas;
+
+    @FXML
+    private Button botaoSair;
+
+    @FXML
+    private Button botaoServico;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private TextField cpfCliente;
+
+    @FXML
+    private TextField enderecoCliente;
+
+    @FXML
+    private TextField nomeCliente;
+
+    @FXML
+    private Label nomeDoUsuario;
+
+    @FXML
+    private TextField sobrenomeCliente;
+
+    @FXML
+    private Pane telaDeCadastroDeCliente;
+
+    @FXML
+    void PesquisarServicos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inserirCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarCarros(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pesquisarPecas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarParaMenu(ActionEvent event) {
+
+    }
+
+}
